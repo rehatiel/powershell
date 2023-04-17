227b72103040c005fa01725c3fb8024281e1db55
rehatiel <rehatiel@gmail.com>
2023-04-17 13:52:31 -0400

    Using .SamAccountName instead of .Name

--- ninja-scripts/rotate-domain-admin.ps1
+++ ninja-scripts/rotate-domain-admin.ps1
@@ -5,7 +5,7 @@ $AdminUser = "mspadmin"
 add-type -AssemblyName System.Web
 $AdminPW = [System.Web.Security.Membership]::GeneratePassword(24,5)
 #Check for the existance of the $AdminUser
-if ((Get-ADUser -Filter *).Name -eq $AdminUser){
+if ((Get-ADUser -Filter *).SamAccountName -eq $AdminUser){
     Set-ADAccountPassword -Identity $AdminUser -NewPassword ($AdminPW | ConvertTo-SecureString -AsPlainText -Force)
     }else{
         New-ADUser -Name $AdminUser -Accountpassword ($AdminPW | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true -Enabled $true
